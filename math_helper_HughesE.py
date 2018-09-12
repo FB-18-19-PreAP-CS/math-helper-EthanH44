25a5e1c2baee1009c6dee2b1ce1822c1cf2ded61
HUGHES.ETHAN <2210321@fbisd.flourbluffschools.net>
2018-09-12 14:06:40 -0500

    initial commit

---
+++ math_helper_HughesE.py
@@ -0,0 +1,20 @@
+    
+    
+
+  
+def slope(x1,y1,x2,y2):
+    y = y1-y2
+    x = x1-x2
+    slope = y/x
+    return slope
+    
+    
+
+
+
+def add(x,y):
+    a = x+y
+    print(a)
+
+slope(3,5,6,7)
+add(53,342)
