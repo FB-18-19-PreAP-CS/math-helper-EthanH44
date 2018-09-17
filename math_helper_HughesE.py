22e7197d020c3afe1d5048f9ff99b76ded67ff48
HUGHES.ETHAN <2210321@fbisd.flourbluffschools.net>
2018-09-17 14:10:34 -0500

    good on doctest for 1 formula

--- math_helper_HughesE.py
+++ math_helper_HughesE.py
@@ -4,26 +4,35 @@ from math import *
   
 def slope(x1,y1,x2,y2):
     ''' Finds how much x increases/decreases and y
-        increases/decreases as a line advances ''' 
+        increases/decreases as a line advancess
+        
+        >>> slope(0,0,1,1)
+        1.0
+    
+        
+    
+    ''' 
     y = y1-y2
     x = x1-x2
+    if x == 0:
+        return "False, 0 for denominator is undefined"
     slope = y/x
     return slope
 
-def Pythag(a,b):
+def pythag(a,b):
     ''' With given two sides of a triangle you will be
         able to find the length of the third side '''
     c = (a**2)+(b**2)
     c = sqrt(c)
     return c
 
-def Arithmetic_last(a1,n,d):
+def arithmetic_last(a1,n,d):
     ''' Gives the nth term in a sequence that is
         arithmetic(addition) '''
     an = a1 + ((n-1)*d)
     return an
 
-def Geo_last(a1,n,r):
+def geo_last(a1,n,r):
     ''' Gives the nth term in a sequence that is
         geometric(multiplication) '''
     an = a1*(r**(n-1))
@@ -40,4 +49,6 @@ def main():
     return
 
 if __name__ == "__main__":
-    main()
+    #main()
+    import doctest
+    doctest.testmod()
